cee45e535ca3085cd4802e8d3381611e6e778195
Tareq Saifullah <tareqsaifu@gmail.com>
2021-05-09 20:01:46 +0200

    Team Controller return team and latest matches played by that team with pagination

--- src/main/java/com/tareq/IPL/controller/TeamController.java
+++ src/main/java/com/tareq/IPL/controller/TeamController.java
@@ -7,7 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
-
+/*Rest Controller*/
 
 @RestController
 public class TeamController {
