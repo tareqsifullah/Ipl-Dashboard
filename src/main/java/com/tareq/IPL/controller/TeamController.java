f28e4d4977a71cbfe285ca18b6556d36f8f2881b
Tareq Saifullah <tareqsaifu@gmail.com>
2021-05-11 07:34:23 +0200

    react app with team page (without css)

--- src/main/java/com/tareq/IPL/controller/TeamController.java
+++ src/main/java/com/tareq/IPL/controller/TeamController.java
@@ -4,12 +4,14 @@ import com.tareq.IPL.model.Team;
 import com.tareq.IPL.repository.MatchRepository;
 import com.tareq.IPL.repository.TeamRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 /*Rest Controller*/
 
 @RestController
+@CrossOrigin
 public class TeamController {
     private TeamRepository teamRepository;
     private MatchRepository matchRepository;
